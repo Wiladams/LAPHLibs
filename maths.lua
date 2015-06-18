2d1e883c0f43a4d459828ab9188004d20a9e1de9
William Adams <spam@nanotechstyles.com>
2015-06-18 15:03:28 -0700

    Added min_bytes_needed() to maths.lua

--- maths.lua
+++ maths.lua
@@ -2,6 +2,8 @@ local bit = require("bit")
 local band, bor = bit.band, bit.bor 
 local rshift, lshift = bit.rshift, bit.lshift
 
+local limits = require("limits")
+
 local floor = math.floor;
 local ceil = math.ceil;
 
@@ -36,8 +38,46 @@ local function roundup32(x)
 	return x
 end
 
+local function min_bytes_needed(value)
+    local bytes;
+    
+    if (value <= limits.UINT32_MAX) then
+        if (value < 16777216) then
+            if (value <= limits.UINT16_MAX) then
+                if (value <= limits.UINT8_MAX) then 
+                    bytes = 1;
+                else 
+                    bytes = 2;
+                end
+            else 
+                bytes = 3;
+            end
+        else 
+            bytes = 4;
+        end
+    
+    elseif (value <= limits.UINT64_MAX) then 
+        if (value < 72057594000000000ULL) then 
+            if (value < 281474976710656ULL) then
+                if (value < 1099511627776ULL) then
+                    bytes = 5;
+                else 
+                    bytes = 6;
+                end
+            else 
+                bytes = 7;
+            end
+        else 
+            bytes = 8;
+        end
+    end
+
+    return bytes;
+end
+
 local exports = {
 	is_power_of_two = is_power_of_two;
+	min_bytes_needed = min_bytes_needed;
 	round = round;
 	roundup = roundup32;
 }
